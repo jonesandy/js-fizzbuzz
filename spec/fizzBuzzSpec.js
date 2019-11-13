3d448aac5e61393c3caea220f90670d4325c89d5
Andy Jones <andytjones@icloud.com>
2019-11-13 10:45:53 +0000

    Refactored method into one.

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,27 +1,26 @@
 describe("fizzbuzz", function(){
-  var javabuzz;
+  beforeEach(function(){
+    javabuzz = new Javabuzz();
+  });
 
   describe('knows when a number is', function(){
 
     it('is divisable by 3', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe("Java");
+      expect(javabuzz._isDivisibleBy(3, 3)).toBe(true);
     });
 
     it('is not divisable by 4', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(4)).toBe(4);
+      expect(javabuzz._isDivisibleBy(3, 4)).toBe(false);
     });
 
     it('is divisable by 5', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByFive(5)).toBe("Buzz");
+      expect(javabuzz._isDivisibleBy(5, 5)).toBe(true);
     });
 
     it('is not divisable by 4', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByFive(4)).toBe(4);
+      expect(javabuzz._isDivisibleBy(4, 5)).toBe(false);
     });
+
 
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -2,20 +2,6 @@ var Javabuzz = function(){
 
 };
 
-Javabuzz.prototype.isDivisibleByThree = function(num) {
-  if (num % 3 === 0){
-    return "Java";
-  } else {
-    return num;
-  }
-  
-};
-
-Javabuzz.prototype.isDivisibleByFive =  function(num) {
-  if (num % 5 === 0){
-    return "Buzz";
-  } else {
-    return num;
-  }
-  
+Javabuzz.prototype._isDivisibleBy = function(num, divisor) {
+  return (num % divisor === 0)
 };
